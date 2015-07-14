97bb9cd970b64f0ce2425ddb959529b011b62fde
Dimitri <dimitri.segard@gmail.com>
2015-07-14 16:40:34 +0200

    Add more unit tests

---
+++ _utils_test.go
@@ -0,0 +1,55 @@
+package randomstring
+
+import "testing"
+
+func TestRangeInt(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestIntToStringRange(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestAppendIntRanges(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestAppendStrRanges(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestPrepend(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestPrependArray(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestShift(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestRandInt(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestRandomString(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestDeleteFromIndex(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestDeleteKeysFromArray(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestArrayToMap(t *testing.T) {
+	t.Error("Fail!")
+}
+
+func TestSmartSplit(t *testing.T) {
+	t.Error("Fail!")
+}
